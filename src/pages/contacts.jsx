61974eba224f12959291415fabbc034b5ff8c421
Viacheslav Plevako <cheryec@ukr.net>
2023-05-09 18:18:52 +0300

    Update contacts.jsx

--- src/pages/contacts.jsx
+++ src/pages/contacts.jsx
@@ -14,7 +14,7 @@ export default function Contacts() {
       >
         <p>Наші координати:</p>
         <p>м. Київ, вул. Велика Кільцева, 4Б,</p>
-        <p>магазин "MG, FAW,JAC, Chery, Geely"</p>
+        <p>магазин (MG, FAW,JAC, Chery, Geely)</p>
         <p>т. 8(097)7934488 т. 8(093)9123221 Viber т. 8(095)5987683</p>
         <p>http://renfa.com.ua/</p>
       </div>
