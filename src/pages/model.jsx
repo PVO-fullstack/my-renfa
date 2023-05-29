ca3481e19a3d0a60c07b6724e0f080cdc0154199
Viacheslav Plevako <cheryec@ukr.net>
2023-05-29 16:01:55 +0300

    Update model.jsx

--- src/pages/model.jsx
+++ src/pages/model.jsx
@@ -6,25 +6,26 @@
 // import { Modal } from "@/component/Modal";
 // // import parts from "../data/350.json";
 
-// export default function Model() {
-//   const [showModal, setShowModal] = useState(false);
-//   const [activeImgIdx, setActiveImgIdx] = useState(null);
-//   const [part, setPart] = useState([]);
+export default function Model() {
+  //   const [showModal, setShowModal] = useState(false);
+  //   const [activeImgIdx, setActiveImgIdx] = useState(null);
+  //   const [part, setPart] = useState([]);
 
-//   const openModal = (index) => {
-//     setShowModal(true);
-//     setActiveImgIdx(index);
-//   };
+  //   const openModal = (index) => {
+  //     setShowModal(true);
+  //     setActiveImgIdx(index);
+  //   };
 
-//   const getParts = (parts) => {
-//     setPart(parts);
-//   };
+  //   const getParts = (parts) => {
+  //     setPart(parts);
+  //   };
 
-//   const closeModal = () => setShowModal(false);
-//   return (
-//     <Layout>
-//       <Part openModal={openModal} getAllParts={getParts} />
-//       {showModal && <Modal src={part[activeImgIdx]} onClose={closeModal} />}
-//     </Layout>
-//   );
-// }
+  //   const closeModal = () => setShowModal(false);
+  return (
+    <Layout>
+      <div>Model</div>
+      {/* <Part openModal={openModal} getAllParts={getParts} />
+      {showModal && <Modal src={part[activeImgIdx]} onClose={closeModal} />} */}
+    </Layout>
+  );
+}
