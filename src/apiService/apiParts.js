3020eb5711baec4a30976f3619b3f88b40d14d67
Viacheslav Plevako <cheryec@ukr.net>
2023-07-15 21:12:12 +0300

    Update apiParts.js

--- src/apiService/apiParts.js
+++ src/apiService/apiParts.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
-// axios.defaults.baseURL = "https://renfa-api.onrender.com";
-axios.defaults.baseURL = "http://localhost:3001";
+axios.defaults.baseURL = "https://renfa-api.onrender.com";
+// axios.defaults.baseURL = "http://localhost:3001";
 // axios.defaults.baseURL = "https://renfa-apiup.vercel.app";
 
 const token = {
