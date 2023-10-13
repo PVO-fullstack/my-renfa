a201de2c4b8caff8afdc2b792ef41c141bb72bdc
Viacheslav Plevako <cheryec@ukr.net>
2023-10-13 14:20:51 +0300

    Change BG

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,20 +6,13 @@ import Navbars from "@/component/NavBar";
 
 export default function Layout({ children }) {
   return (
-    // <html lang="en">
-    //   <head></head>
-    //   <body style={{ backgroundColor: "GrayText" }}>
     <>
       <Navbars />
       <div className={css.main}>
-        {/* <div> */}
-        {/* <div style={{ display: "flex" }}> */}
         <SideBar />
         {children}
       </div>
-      {/* </div> */}
       <Footer />
-      {/* </div> */}
     </>
   );
 }
