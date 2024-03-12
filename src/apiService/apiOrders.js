0de927a743a38635ea4b6480fa269d86bcb26aa3
Viacheslav Plevako <cheryec@ukr.net>
2024-03-12 12:25:03 +0200

    env

--- src/apiService/apiOrders.js
+++ src/apiService/apiOrders.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
-BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 axios.defaults.baseURL = BASE_URL;
 // axios.defaults.baseURL = "http://localhost:3001";
