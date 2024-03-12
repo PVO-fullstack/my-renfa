3735471cde48d5f4831d475fda4268a5475bdd82
Viacheslav Plevako <cheryec@ukr.net>
2024-03-12 12:03:35 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,12 +3,6 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-  webpack5: true,
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false };
-
-    return config;
-  },
   images: {
     deviceSizes: [480, 768, 1024, 1280, 1366, 1440, 1920, 2048],
     formats: ["image/avif", "image/webp"],
